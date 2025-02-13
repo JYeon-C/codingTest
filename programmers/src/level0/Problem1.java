4bf2002d95267a881c06fbd1da622513a324c667
rogiwim <rogiwim@naver.com>
2025-02-14 00:19:11 +0900

    프로그래머스 0단계(1)

---
+++ programmers/src/level0/Problem1.java
@@ -0,0 +1,23 @@
+package level0;
+
+import java.util.Scanner;
+
+class Problem1 {
+
+    // 나머지 결과 출력
+    public static int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1 % num2;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(solution(num1, num2));
+    }
+}
