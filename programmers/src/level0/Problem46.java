56db26c1e6d3d18e22378f391ff93a451aa8c80e
rogiwim <rogiwim@naver.com>
2025-02-22 20:15:04 +0900

    프로그래머스 0단계(6)

---
+++ programmers/src/level0/Problem46.java
@@ -0,0 +1,17 @@
+package level0;
+
+import java.util.ArrayList;
+
+public class Problem46 {
+    public ArrayList<Integer> solution(int n) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for(int i = 1; i <= n; i ++) {
+            if(n % i == 0) {
+                answer.add(i);
+            }
+        }
+
+        return answer;
+    }
+}
