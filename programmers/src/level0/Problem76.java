1d92fba9b04610765a91756feaffd1ff49fed5fd
jyeonchoi <rogiwim@naver.com>
2025-03-23 17:03:56 +0900

    프로그래머스 0단계(19)

--- programmers/src/level0/Problem76.java
+++ programmers/src/level0/Problem76.java
@@ -1,7 +1,15 @@
 package level0;
 
+// 공 던지기
 public class Problem76 {
-    public static void main(String[] args) {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        int index = 0;
 
+        for(int i = 2; i <= k; i++) {
+            index = ((index + 2) % numbers.length);
+        }
+        answer = numbers[index];
+        return answer;
     }
 }
