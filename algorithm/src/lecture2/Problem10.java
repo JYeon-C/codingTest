deaf2680b57ae07585afd496f5c52c7f9022c696
최정연 <rogiwim@naver.com>
2025-10-31 23:56:01 +0900

    [자바 알고리즘 문제 풀이] 강의 2 - 문제 10번

---
+++ algorithm/src/lecture2/Problem10.java
@@ -0,0 +1,41 @@
+package lecture2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem10 {
+    public int solution(int[][] inputs) {
+        int answer = 0;
+
+        for(int i = 0; i < inputs.length; i++) {
+            for(int j = 0; j < inputs.length; j++) {
+                if(i != 0 && j != 0 && i != inputs.length -1 && j != inputs.length - 1) {
+                    int temp = inputs[i][j];
+                    if(inputs[i-1][j] < temp && inputs[i+1][j] < temp && inputs[i][j-1] < temp && inputs[i][j+1] < temp) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Problem10 T = new Problem10();
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] inputs = new int[N+2][N+2];
+
+        for(int i = 0; i < inputs.length; i++) {
+            for(int j = 0; j < inputs.length; j++) {
+                if(i == 0 || j == 0 || i == inputs.length -1 || j == inputs.length - 1) {
+                    inputs[i][j] = 0;
+                } else {
+                    inputs[i][j] = sc.nextInt();
+                }
+            }
+        }
+        System.out.println(T.solution(inputs));
+    }
+}
